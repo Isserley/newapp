6675b05fc412d8218d2fe9ec055bd5486997c146
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-06-02 22:15:34 -0600

    corrected links in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  # get 'static_pages/home'
 
-  get 'static_pages/about'
+  # get 'static_pages/about'
+root 'static_pages#home'
+
+get '/about', to: 'static_pages#about', as: :about
 
   # get 'posts/index'
 
